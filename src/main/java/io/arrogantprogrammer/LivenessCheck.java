b19b30e957ff4a7eee34d2a2a77792a00e23976f
jeremyrdavis <jeremyrdavis@mac.com>
2024-06-05 15:17:06 -0500

    added health check

---
+++ src/main/java/io/arrogantprogrammer/LivenessCheck.java
@@ -0,0 +1,15 @@
+package io.arrogantprogrammer;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+
+@Liveness
+@ApplicationScoped
+public class LivenessCheck implements HealthCheck {
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.up("Simple health check");
+    }
+}
